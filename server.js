c1bcfda82f92644f57f1c661a04a1fd14613ccbb
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-02 14:51:40 -0800

    hello world init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
