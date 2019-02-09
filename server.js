8d25a4ba10dd4e8f4af8cf891a0a1e8ad85e0c07
Nyapal Gatkuoth <nyapal14@gmail.com>
2019-02-09 14:41:45 -0800

    users can sign up

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 const express = require('express')
 const exphbs = require('express-handlebars')
+
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 const app = express()
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
@@ -9,11 +14,13 @@ app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(expressValidator());
 
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 require('./data/reddit-db');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
